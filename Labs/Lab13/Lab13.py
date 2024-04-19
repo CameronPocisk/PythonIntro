4cda5ab3bad8d29fc408cb6036c983527cc2ad14
CameronPocisk <cameronpocisk@gmail.com>
2024-04-19 12:27:24 -0400

    start last lab (13)

---
+++ Labs/Lab13/Lab13.py
@@ -0,0 +1,14 @@
+# For this assignment you are to write code to run a simulation for each value of N from 1 to 1000, 
+import random
+
+# and plot the results using the matplotlib plot function.
+
+
+# We would like to know the slope of this linear relationship.
+
+
+# Write code to run a linear regression to find the best fit line for your dataset. Your output should be similar to the following:
+# SciPy Linear Regression Solution 
+#  slope:  0.6325801874018306
+#  intercept: -0.02602000798390236        
+#  rvalue: 0.997749733854947
